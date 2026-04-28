654544b152c0bc60f44b82146d7405f65a0eb4e3
fazeelamushtaq <fazeela.mushtaq.sirius@gmail.com>
2026-04-28 20:14:23 +0500

    Automate Serverless Deploymnet using AWS Lambda and Test APIs

---
+++ lambda.js
@@ -0,0 +1,9 @@
+import awsServerlessExpress from "aws-serverless-express"
+import app from "./index.js"
+
+const server = awsServerlessExpress.createServer(app)
+
+export const handler = (event, context) => {
+    return awsServerlessExpress.proxy(server, event, context)
+    
+}
